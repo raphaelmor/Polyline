771c155fac403a1a44feea5b9ad236b41dc04fc0
Raphaël MOR <rmor@tp.michelin.com>
2014-09-02 13:57:47 +0200

    initial import

--- Polyline/Polyline.h
+++ Polyline/Polyline.h
@@ -1,19 +1,31 @@
+// The MIT License (MIT)
 //
-//  Polyline.h
-//  Polyline
+// Copyright (c) 2014 Raphaël Mor
 //
-//  Created by Raphael MOR on 02/09/2014.
-//  Copyright (c) 2014 Raphael MOR. All rights reserved.
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
 //
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 #import <UIKit/UIKit.h>
 
-//! Project version number for Polyline.
+// ! Project version number for Polyline.
 FOUNDATION_EXPORT double PolylineVersionNumber;
 
-//! Project version string for Polyline.
+// ! Project version string for Polyline.
 FOUNDATION_EXPORT const unsigned char PolylineVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Polyline/PublicHeader.h>
-
-
