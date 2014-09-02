0b1313377608de39cd748dc0b1e321a292eb39af
Raphaël MOR <rmor@tp.michelin.com>
2014-09-02 13:20:31 +0200

    Initial Commit

---
+++ Polyline/Polyline.h
@@ -0,0 +1,19 @@
+//
+//  Polyline.h
+//  Polyline
+//
+//  Created by Raphael MOR on 02/09/2014.
+//  Copyright (c) 2014 Raphael MOR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Polyline.
+FOUNDATION_EXPORT double PolylineVersionNumber;
+
+//! Project version string for Polyline.
+FOUNDATION_EXPORT const unsigned char PolylineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Polyline/PublicHeader.h>
+
+
